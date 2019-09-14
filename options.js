f3974c9fb82ea0fc40e0725daf7ae6dd631f7189
cail <cail1844@gmail.com>
2019-09-14 12:05:00 +0800

    在option页面生成分类选项

--- options.js
+++ options.js
@@ -1,5 +1,12 @@
 'use strict';
 
+
+const createItemsEleFromJson = (text) => {
+  let cats = new Map(JSON.parse(text))
+  let itemsEle = createItemsEle(cats)
+  app.appendChild(itemsEle)
+}
+
 let app = document.querySelector('#app');
 
 function createEleFromHTML(htmlString) {
@@ -12,7 +19,7 @@ function createItemsEle(cats) {
     let itemsEle = createEleFromHTML('<div class="items"></div>')
     for (let [id, name] of cats.entries()) {
         let htmlString = `<div class="item">
-        <input type="checkbox" id="${id}">
+        <input type="checkbox" name="cats" value="${id}">
         <label for="${id}">${name}</label>
       </div>`
 
@@ -21,6 +28,12 @@ function createItemsEle(cats) {
     return itemsEle
 }
 
-let cats = new Map().set('id', 'name')
-let itemsEle = createItemsEle(cats)
-app.appendChild(itemsEle)
+const url = chrome.runtime.getURL('data/cats.json')
+
+fetch(url)
+    .then((response) => response.text())
+    .then((text) => createItemsEleFromJson(text));
+
+// let cats = new Map().set('id', 'name')
+// let itemsEle = createItemsEle(cats)
+// app.appendChild(itemsEle)
